a8cdb38de169c65a979878edeffc18fa83ec20dc
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-07-08 15:35:02 +0200

    one type of exports only to reduce cognitive overhead

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // TASK 1- Import the data we need to "hydrate" our component
 //  On the one hand, the default export from data/panelData.js
-//  On the other hand, two NAMED exports (open and close) from data/constants.js
+//  On the other hand, the default export from data/constants.js
 import panelData from './data/panelData.js'
-import { open, close } from './data/constants.js'
+import constants from './data/constants.js'
 
+const { open, close } = constants
 
 // TASK 2- Verify our imports using log statements
 // console.log() // log the title meant for the first panel
